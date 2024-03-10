a390d2b7d490d60be37623c95140a4be4d644dae
th3play3r <96393191+th3play3r@users.noreply.github.com>
2024-03-10 14:11:36 +0200

    Add files via upload

---
+++ GlobalTicketHub/globaltickethub.client/src/Components/AuthForm/AuthForm.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './AuthForm.css';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+
+class AuthForms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeForm: 'login',
+    };
+  }
+
+  handleFormSwitch = (newForm) => {
+    this.setState({ activeForm: newForm });
+  };
+
+  render() {
+    return (
+      <div className="auth-container">
+        <div className="auth-card">
+          <div className="buttons-container">
+            <button
+              onClick={() => this.handleFormSwitch('register')}
+              className={this.state.activeForm === 'login' ? 'active' : ''}
+            >
+              Реєстрація
+            </button>
+            <button
+              onClick={() => this.handleFormSwitch('login')}
+              className={this.state.activeForm === 'register' ? 'active' : ''}
+            >
+              Аутентифікація
+            </button>
+
+            {/* Place the forms below the buttons */}
+            <div className="forms-container">
+              {this.state.activeForm === 'login' ? <LoginForm /> : <RegisterForm />}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AuthForms;
