aa6bc815529512e623a7e83d80389e91a074efa7
Dosdafter1 <gfock57@gmail.com>
2024-05-11 09:12:58 +0300

    wagonType update

--- GlobalTicketHub/globalticket.client/src/enums/wagon/wagonTypeEnum.js
+++ GlobalTicketHub/globalticket.client/src/enums/wagon/wagonTypeEnum.js
@@ -3,7 +3,8 @@ import { useTranslation } from "react-i18next"
 export const WagonType = () =>{
     const {t} = useTranslation()
     return {
-    "0":t('wagonType.seatFirst'),
-    "1":t('wagonType.seatSecond'),
-    "2":t('wagonType.plac'),}
+    "0":t('wagonType.plac'),
+    "1":t('wagonType.seatFirst'),
+    "2":t('wagonType.seatSecond'),
+    }
 }
